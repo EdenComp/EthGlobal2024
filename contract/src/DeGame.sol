aaa1b440716daa51f0455069ffeb67f88a377d9f
Florian Lauch <florian.lauchh@gmail.com>
2024-07-13 21:06:57 +0200

    feat: Smart contract additional features

--- contract/src/DeGame.sol
+++ contract/src/DeGame.sol
@@ -11,19 +11,32 @@ contract DeGame is EIP712WithModifier {
         uint8 dieValue;
     }
 
+    struct Round {
+        Turn[] turns;
+        bool liar;
+    }
+
     struct Game {
         uint256 id;
         address owner;
         address[] alivePlayers;
         uint8 turnPlayerIndex;
-        Turn[][] rounds;
+        uint16 roundNumber;
+        uint16 turnNumber;
+        Round[] rounds;
     }
 
-    event GameCreated (uint256 gameId, address creator);
+    struct AvailableGame {
+        uint256 id;
+        address creator;
+        uint8 playerCount;
+    }
+
+    event GameCreated(uint256 gameId, address creator);
     event GameStarted(uint256 gameId);
     event TurnStarted(uint256 gameId);
     event DiceCallMade(uint256 gameId, address player, uint16 nbDice, uint8 dieValue);
-    event LiarCallMade(uint256 gameId, address player, uint16 nbDice, uint8 dieValue);
+    event LiarCallMade(uint256 gameId, address player, uint16 nbDice, uint8 dieValue, bool liar);
     event TurnEnded(uint256 gameId, address loser);
     event GameEnded(uint256 gameId, address winner);
 
@@ -33,18 +46,19 @@ contract DeGame is EIP712WithModifier {
     mapping(uint256 => Game) public games;
 
     uint256 private nonce = 0;
+    mapping(address => uint256) public playerGame;
     mapping(uint256 => mapping(address => euint8[])) private playerDice;
 
     constructor() EIP712WithModifier("DeGame", "1") {}
 
-    function getAvailableGames() public view returns (uint256[] memory) {
-        uint256[] memory availableGames = new uint256[](gameIds.length);
+    function getAvailableGames() public view returns (AvailableGame[] memory) {
+        AvailableGame[] memory availableGames = new AvailableGame[](gameIds.length);
         uint index = 0;
 
         for (uint i = 0; i < gameIds.length; i++) {
             Game memory game = games[gameIds[i]];
-            if (game.rounds.length == 0) {
-                availableGames[index] = game.id;
+            if (game.roundNumber == 0) {
+                availableGames[index] = AvailableGame(game.id, game.owner, uint8(game.alivePlayers.length));
                 index++;
             }
         }
@@ -55,10 +69,14 @@ contract DeGame is EIP712WithModifier {
         return games[gameId];
     }
 
+    function getPlayerGame() public view returns (Game memory) {
+        return games[playerGame[msg.sender]];
+    }
+
     function getDice(uint256 gameId, bytes32 publicKey, bytes calldata signature) public view onlySignedPublicKey(publicKey, signature) returns (bytes[] memory) {
         Game storage game = games[gameId];
         require(game.owner != address(0), "Game does not exist");
-        require(game.rounds.length > 0, "Game did not start");
+        require(game.roundNumber > 0, "Game did not start");
         require(game.alivePlayers.length > 1, "Game ended");
         require(playerDice[gameId][msg.sender].length > 0, "You are not in the game");
 
@@ -70,30 +88,54 @@ contract DeGame is EIP712WithModifier {
     }
 
     function createGame() public {
+        require(playerGame[msg.sender] == 0, "You are already in a game");
+
         uint256 id = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce++)));
         gameIds.push(id);
         games[id].id = id;
         games[id].owner = msg.sender;
         games[id].alivePlayers.push(msg.sender);
+        games[id].roundNumber = 0;
+        games[id].turnNumber = 0;
         playerDice[id][msg.sender] = new euint8[](DICE_NUMBER);
 
         emit GameCreated(id, msg.sender);
     }
 
     function joinGame(uint256 gameId) public {
+        require(playerGame[msg.sender] == 0, "You are already in a game");
         Game storage game = games[gameId];
         require(game.owner != address(0), "Game does not exist");
-        require(game.rounds.length == 0, "Game already started");
+        require(game.roundNumber == 0, "Game already started");
         require(playerDice[game.id][msg.sender].length == 0, "Already joined");
+
         game.alivePlayers.push(msg.sender);
         playerDice[game.id][msg.sender] = new euint8[](DICE_NUMBER);
+    }
+
+    function leaveGame() public {
+        // TODO: Can leave a game that has started?
+        require(playerGame[msg.sender] != 0, "You are not in a game");
+
+        Game storage game = games[playerGame[msg.sender]];
+        require(game.roundNumber == 0, "Game already started");
+        require(game.owner != msg.sender, "The owner cannot leave the game");
+        require(game.roundNumber == 0, "The game has already started");
+
+        delete playerGame[msg.sender];
+        for (uint8 i = 0; i < game.alivePlayers.length; i++) {
+            if (game.alivePlayers[i] == msg.sender) {
+                removePlayer(game, i);
+                break;
+            }
+        }
     }
 
     function startGame(uint256 gameId) public {
         Game storage game = games[gameId];
         require(game.owner != address(0), "Game does not exist");
         require(game.owner == msg.sender, "Only the owner can start the game");
-        require(game.rounds.length == 0, "Game already started");
+        require(game.roundNumber == 0, "Game already started");
         require(game.alivePlayers.length > 1, "Not enough players");
 
         emit GameStarted(game.id);
@@ -105,22 +147,28 @@ contract DeGame is EIP712WithModifier {
         require(dieValue >= 1 && dieValue <= 6, "Die value must be between 1 and 6");
         require(nbDice > 0, "Number of dice must be greater than 0");
 
-        Turn[] storage lastRound = game.rounds[game.rounds.length - 1];
-        Turn storage lastTurn = lastRound[lastRound.length - 1];
-        require(nbDice > lastTurn.nbDice || dieValue > lastTurn.dieValue, "You must increase the number of dice or the die value");
+        Round storage lastRound = game.rounds[game.rounds.length - 1];
+        if (game.turnNumber > 0) {
+            Turn storage lastTurn = lastRound.turns[game.turnNumber - 1];
+            require(nbDice > lastTurn.nbDice || dieValue > lastTurn.dieValue, "You must increase the number of dice or the die value");
+        }
 
-        lastRound.push(Turn(msg.sender, nbDice, dieValue));
+        game.rounds[game.roundNumber - 1].turns[game.turnNumber - 1] = Turn(msg.sender, nbDice, dieValue);
+        game.turnNumber += 1;
         game.turnPlayerIndex = uint8((game.turnPlayerIndex + 1) % game.alivePlayers.length);
         emit DiceCallMade(game.id, msg.sender, nbDice, dieValue);
     }
 
     function makeLiarCall(uint256 gameId) public turnBased(gameId) {
         Game storage game = games[gameId];
-        Turn[] storage lastRound = game.rounds[game.rounds.length - 1];
-        Turn storage lastTurn = lastRound[lastRound.length - 1];
+        require (game.turnNumber > 0, "You must make a dice call first");
 
-        endTurn(game, isLiar(game, lastTurn.nbDice, lastTurn.dieValue));
-        emit LiarCallMade(game.id, msg.sender, lastTurn.nbDice, lastTurn.dieValue);
+        Turn storage lastTurn = game.rounds[game.roundNumber - 1].turns[game.turnNumber - 1];
+        bool liar = isLiar(game, lastTurn.nbDice, lastTurn.dieValue);
+        game.rounds[game.roundNumber - 1].liar = liar;
+
+        emit LiarCallMade(game.id, msg.sender, lastTurn.nbDice, lastTurn.dieValue, liar);
+        endTurn(game, liar);
     }
 
     function isLiar(Game storage game, uint16 nbDice, uint16 dieValue) private view returns (bool) {
@@ -150,14 +198,17 @@ contract DeGame is EIP712WithModifier {
     modifier turnBased(uint256 gameId) {
         Game storage game = games[gameId];
         require(game.owner != address(0), "Game does not exist");
-        require(game.rounds.length > 0, "Game not started");
+        require(game.roundNumber > 0, "Game not started");
         require(game.alivePlayers.length > 1, "Game ended");
+        // TODO: Specific time to play
         require(game.alivePlayers[game.turnPlayerIndex] == msg.sender, "Not your turn");
         _;
     }
 
     function startTurn(Game storage game) private {
-        game.rounds.push();
+        game.roundNumber += 1;
+        game.turnNumber = 0;
+
         for (uint8 i = 0; i < game.alivePlayers.length; i++) {
             for (uint j = 0; j < playerDice[game.id][game.alivePlayers[i]].length; j++) {
                 playerDice[game.id][game.alivePlayers[i]][j] = TFHE.add(TFHE.randEuint8(), TFHE.asEuint8(1));
@@ -174,6 +225,7 @@ contract DeGame is EIP712WithModifier {
         emit TurnEnded(game.id, game.alivePlayers[loserPlayerIndex]);
         playerDice[game.id][game.alivePlayers[loserPlayerIndex]].pop();
         if (eliminated) {
+            delete playerGame[game.alivePlayers[loserPlayerIndex]];
             removePlayer(game, loserPlayerIndex);
             game.turnPlayerIndex = uint8(loserPlayerIndex % game.alivePlayers.length);
         } else if (liar) {
@@ -184,6 +236,7 @@ contract DeGame is EIP712WithModifier {
             startTurn(game);
         } else {
             emit GameEnded(game.id, game.alivePlayers[0]);
+            delete playerGame[game.alivePlayers[0]];
         }
     }
 
