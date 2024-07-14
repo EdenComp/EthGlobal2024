d23caf6358512339e660d83274dcccf910f48631
mohammed <molaryyios@gmail.com>
2024-07-14 02:50:27 +0200

    feat: added home footer

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -1,13 +1,44 @@
 import type { ReactElement } from "react";
+import githubLogo from "@/assets/icons/logos/Github.png"
+import discordLogo from "@/assets/icons/logos/Discord.png"
+import telegramLogo from "@/assets/icons/logos/Telegram.png"
+import twitterLogo from "@/assets/icons/logos/Twitter.png"
 
 export default function Footer(): ReactElement {
 	return (
-		<footer className="flex h-16 w-full items-center justify-center bg-background-primary">
-			<div
-				className={"flex items-center justify-center"}
-				style={{ width: "1440px" }}
-			>
-				Footer
+		<footer className="items-center bg-background-primary">
+			<div className={"flex h-16 w-full  main-home-container"}>
+				<div className={"ml-24 w-52 h-2 flex items-center space-x-4"}>
+					<a href={"https://github.com/EdenComp/EthGlobal2024"}>
+						<img src={githubLogo} alt={"logo"}/>
+					</a>
+					<a href={"https://discord.gg/Md5qNKvT"}>
+						<img src={discordLogo} alt={"logo"}/>
+					</a>
+					<a href={"https://t.me/+KYYwUL8FxP5jZjJk"}>
+						<img src={telegramLogo} alt={"logo"}/>
+					</a>
+					<a href={"https://twitter.com/PoCInnovation"}>
+						<img src={twitterLogo} alt={"logo"}/>
+					</a>
+				</div>
+				<div className={"info-container flex space-x-20 items-center"}>
+					<div className={"about-container text-center"}>
+						<p className={"font-bold"}>About</p>
+						<p><a href={"/team"}>Team</a></p>
+					</div>
+					<div className={"resources-container text-center"}>
+						<p className={"font-bold"}><a>Resources</a></p>
+						<p><a>Docs</a></p>
+					</div>
+					<div className={"contact-container text-center"}>
+						<p className={"font-bold"}>Contact</p>
+						<p><a>Contact us!</a></p>
+					</div>
+				</div>
+			</div>
+			<div className={"flex items-center justify-center"}>
+				<p className={"rights-p mt-32"}>@déGame 2024. All rights reserved</p>
 			</div>
 		</footer>
 	);
--- client/src/pages/Home/HeroBannerSection.tsx
+++ client/src/pages/Home/HeroBannerSection.tsx
@@ -1,7 +1,20 @@
 import bgImage from "@/assets/web3Home.jpg";
 import type { ReactElement } from "react";
+import {useEffect, useState} from "react";
+import diceImage from "@/assets/dice-home.jpg";
+import "./Home.css";
 
 const HeroBannerSection = (): ReactElement => {
+	const [isOnDie, setIsOnDie] = useState<boolean>(false);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIsOnDie((prev) => !prev);
+		}, 3000);
+
+		return () => clearInterval(interval);
+	}, [isOnDie]);
+
 	return (
 		<div
 			className="relative flex items-center justify-center w-screen h-[800px] bg-cover bg-center"
@@ -18,7 +31,15 @@ const HeroBannerSection = (): ReactElement => {
 						"flex flex-col w-full h-1/2 bg-opacity-20 bg-neutral-grey_1 rounded-3xl backdrop-blur-2xl items-center justify-center border-t border-neutral-white space-y-8"
 					}
 				>
-					<h1 className={"text-9xl font-semibold"}>DéGame</h1>
+					<div className={"flex flex-row items-center title-container"}>
+						{isOnDie == false ?
+							<h1 className={"text-9xl font-semibold title transition-opacity ease-in-out"}>DéGame</h1>
+							: <>
+								<img src={diceImage} alt={"dice home image"} className={"w-32 ml-9 image-dice transition-transform"}/>
+								<h1 className={"text-9xl font-semibold"}>Game</h1>
+							</>
+						}
+					</div>
 					<p>
 						Enter the world of Decentralized Liar's Dice on the blockchain:
 						where bluff meets blockchain, and trust is coded into every roll.
